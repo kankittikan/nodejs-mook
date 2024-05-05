f7338b95afc4232544cd335a491b1c35d8bab1a6
kankittikan <nongkan195@gmail.com>
2024-05-05 22:08:09 +0700

    refactor controller

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const mysql = require('mysql2/promise')
 const bcrypt = require('bcrypt')
 const cors = require('cors')
 
+//import controlers
+const loginController = require('./controllers/loginController')
+
 const corsOptions = {
     origin: 'http://localhost:3000',
     credentials : true
@@ -15,7 +18,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(cors(corsOptions));
 
-let conn = null
+global.conn = null
 
 // init db
 const initDB = async () => {
@@ -27,48 +30,8 @@ const initDB = async () => {
     })
 }
 
-// api
-app.post('/api/login', async (req, res) => {
-    try {
-        const { user, password } = req.body
-
-        const [results] = await conn.query('select * from employee where User = ?', user)
-
-        if (!results[0]) {
-            res.status(401).json({
-                message: "ไม่พบผู้ใช้"
-            })
-            return false
-        }
-
-        const userData = results[0]
-        const valid = await bcrypt.compare(password, userData.password)
-
-        if (!valid) {
-            res.status(401).json({
-                message: "รหัสผ่านไม่ถูกต้อง"
-            })
-            return false
-        }
-
-        // sign jwt
-        const token = jwt.sign({
-            userId: userData.EN,
-            name: userData.name + ' ' + userData.last_name
-        }, process.env.SECRET, { expiresIn: '1h' })
-
-        res.cookie('token', token)
-
-        res.json({
-            message: 'OK'
-        })
-    } catch (error) {
-        res.status(500).json({
-            message: "Server Error",
-            error
-        })
-    }
-})
+// controller
+app.post('/api/login', loginController)
 
 // main
 app.listen(process.env.PORT, async () => {
